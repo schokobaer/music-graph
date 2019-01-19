acfc571e1ccb9f548a484e82190816be804528a6
Josef Wechselauer <wechselauer.josef@gmx.at>
2019-01-19 13:55:46 +0100

    added sparql query example for getting all artists. example for getting upcoming events for found artists added

--- src/main/java/at/ac/tuwien/semanticsystems/musicgraph/Application.java
+++ src/main/java/at/ac/tuwien/semanticsystems/musicgraph/Application.java
@@ -9,6 +9,7 @@ import github.jjbinks.bandsintown.api.BITAPI;
 import github.jjbinks.bandsintown.api.BITAPIClient;
 import github.jjbinks.bandsintown.dto.Artist;
 import github.jjbinks.bandsintown.dto.ArtistEvent;
+import github.jjbinks.bandsintown.exception.BITException;
 import github.jjbinks.bandsintown.impl.BITAPIClientImpl;
 import github.jjbinks.bandsintown.impl.BITAPIImpl;
 import github.jjbinks.bandsintown.impl.resource.ArtistEventsResource;
@@ -19,6 +20,7 @@ import org.apache.jena.query.Dataset;
 import org.apache.jena.query.QueryExecution;
 import org.apache.jena.query.QuerySolution;
 import org.apache.jena.query.ResultSet;
+import org.apache.jena.rdf.model.Literal;
 import org.apache.jena.rdf.model.Model;
 import org.apache.jena.rdf.model.ModelFactory;
 import org.apache.jena.rdf.model.Resource;
@@ -36,6 +38,7 @@ import org.springframework.context.annotation.Bean;
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -60,7 +63,7 @@ public class Application {
             List<YoutubeHistoryParser.YoutubeVideo> youtubeVideos = youtubeHistoryParser.parseFile(youtubeHistoryFilePath);
             LOGGER.info("Found Videos in youtube history: {}", youtubeVideos.size());
 
-            for (YoutubeHistoryParser.YoutubeVideo video: youtubeVideos) {
+            for (YoutubeHistoryParser.YoutubeVideo video : youtubeVideos) {
                 // Query for releases on musicbrainz
                 List<JSONObject> queryResults = musicbrainzService.search(video.getVideoTitle());
                 if (queryResults.isEmpty()) {
@@ -95,52 +98,83 @@ public class Application {
             dataModel.write(System.out, "TURTLE");
 
             /*
-            FUSEKI connection
+            FUSEKI connection example
              */
 
             // Setup and start Fuseki
             Dataset ds = new DatasetImpl(dataModel);
             FusekiServer server = FusekiServer.create()
                     .add("/dataset", ds)
-                    .build() ;
-            server.start() ;
+                    .build();
+            server.start();
 
             RDFConnection conn = RDFConnectionFactory.connect(ds);
-            conn.load(dataModel) ;
-            QueryExecution qExec = conn.query("SELECT DISTINCT ?s { ?s ?p ?o }") ;
-            ResultSet rs = qExec.execSelect() ;
-            while(rs.hasNext()) {
-                QuerySolution qs = rs.next() ;
-                Resource subject = qs.getResource("s") ;
-                System.out.println("Subject: "+subject) ;
+            conn.load(dataModel);
+            QueryExecution qExec = conn.query("PREFIX schema: <http://schema.org/> " +
+                    "SELECT DISTINCT ?artist " +
+                    "WHERE  { " +
+                    "?song a schema:MusicRelease. " +
+                    "?song schema:creditedTo ?artist. " +
+                    "}");
+            ResultSet rs = qExec.execSelect();
+
+            // SPARQL query example for all artists in model
+
+            List<String> artistNameList = new ArrayList<>();
+            while (rs.hasNext()) {
+                QuerySolution qs = rs.next();
+                Literal artistName = qs.getLiteral("artist");
+//                System.out.println("Artist: " + artistName);
+                artistNameList.add(artistName.toString());
             }
-            qExec.close() ;
-            conn.close() ;
+            qExec.close();
+            conn.close();
 
             server.stop();
 
             /*
-            BandsInTOwn API Connection example
+            BandsInTown API Connection example
             */
 
             BITAPI bitapi = new BITAPIImpl("JJBinksTest");
             Client client = ClientBuilder.newClient();
             BITAPIClient bitapiClient = new BITAPIClientImpl(client, "JJBinksTest");
 
-            // get artist information
-            Artist artist =  bitapiClient.getBITResource(new ArtistInfoResource("Metallica"));
-            System.out.println(artist.getName() + " " + artist.getUrl());
-            LocalDate fromDate = LocalDate.now();
-            LocalDate toDate = fromDate.plusYears(10);
 
             // list events for artist
-            List<ArtistEvent> artistEvents =  bitapiClient.getBITResource(new ArtistEventsResource("Metallica", fromDate, toDate));
-            for (ArtistEvent event : artistEvents) {
-                System.out.println("Venue: " + event.getVenue().getCountry() + " - " + event.getVenue().getCity() + "\n" +
-                        "Date: " + event.getDatetime() + "\n" +
-                        "Description: " + event.getDescription() + "\n" +
-                        "Event-Link" + event.getUrl() + "\n" +
-                        "-----------------------");
+
+            for (String artistName : artistNameList) {
+
+                System.out.println("****************" + artistName + "****************");
+
+                // example get artist information
+
+                try {
+                    Artist artist = bitapiClient.getBITResource(new ArtistInfoResource(artistName));
+                    System.out.println(artist.getName() + " " + artist.getUrl());
+
+                } catch (BITException e) {
+                    System.err.println("No information for this artist found");
+                }
+
+                // example get upcoming events for artist
+
+                System.out.println("--> UPCOMING EVENTS:");
+                LocalDate fromDate = LocalDate.now();
+                LocalDate toDate = fromDate.plusYears(10);
+                try {
+                    List<ArtistEvent> artistEvents = bitapiClient.getBITResource(new ArtistEventsResource(artistName, fromDate, toDate));
+                    for (ArtistEvent event : artistEvents) {
+                        System.out.println("Venue: " + event.getVenue().getCountry() + " - " + event.getVenue().getCity() + "\n" +
+                                "Date: " + event.getDatetime() + "\n" +
+                                "Description: " + event.getDescription() + "\n" +
+                                "Event-Link" + event.getUrl() + "\n" +
+                                "-----------------------");
+                    }
+                } catch (BITException e) {
+                    System.err.println("No events for this artist found");
+                }
+
             }
         };
     }
