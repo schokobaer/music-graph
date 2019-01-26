27b23e2c46f66927ceea23bb35446cc77dceeec8
Dominik Scheffknecht <e1426857@student.tuwien.ac.at>
2019-01-26 21:04:20 +0100

    impl getSimilarArtists

--- src/main/java/at/ac/tuwien/semanticsystems/musicgraph/service/WikiDataQueryService.java
+++ src/main/java/at/ac/tuwien/semanticsystems/musicgraph/service/WikiDataQueryService.java
@@ -1,5 +1,8 @@
 package at.ac.tuwien.semanticsystems.musicgraph.service;
 
+import org.apache.jena.rdf.model.Model;
+import org.apache.jena.rdf.model.Statement;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.HashMap;
@@ -9,16 +12,23 @@ import java.util.Map;
 public class WikiDataQueryService {
 
 
+    @Autowired
+    private WikidataService wikidataService;
+    @Autowired
+    private MusicbrainzService musicbrainzService;
 
 
     public Map<String, String> getSimilarArtistsGenre(String artistName) {
-        Map<String, String> similarArtists = new HashMap<>();
-        //TODO query artists
-        /* dummy impl */
-        similarArtists.put("Alter Bridge", "wd:Q335036");
-        similarArtists.put("Die Toten Hosen", "wd:Q32461");
-        similarArtists.put("Billy Talent", "wd:Q154815");
-        return similarArtists;
+
+        /* resolve wikidataURI */
+        String wikidataId = musicbrainzService.getWikidataResourceByArtistName("Alter Bridge");
+        wikidataId = "wd:" + wikidataId.substring(wikidataId.lastIndexOf('Q'));
+
+        Map<String, String> params = new HashMap<>();
+        params.put("$paramArtist", wikidataId);
+
+        return params;
+
     }
 
     public Map<String, String> getSimilarArtistsCountry(String artistName) {
@@ -49,5 +59,9 @@ public class WikiDataQueryService {
         similarArtists.put("In the End", "wd:Q20003");
         similarArtists.put("Rusted from the Rain","Q7382465");
         return similarArtists;
+    }
+
+    public void setWikidataService(WikidataService wikidataService) {
+        this.wikidataService = wikidataService;
     }
 }
