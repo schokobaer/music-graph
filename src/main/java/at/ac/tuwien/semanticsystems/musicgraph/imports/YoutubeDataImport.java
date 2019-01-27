cd86b62a7a071495854d51abf95861177f92e3a8
schokobaer <andreas.peter.fend@gmail.com>
2019-01-27 12:51:09 +0100

    Fixed some little musicbrainz bugs

--- src/main/java/at/ac/tuwien/semanticsystems/musicgraph/imports/YoutubeDataImport.java
+++ src/main/java/at/ac/tuwien/semanticsystems/musicgraph/imports/YoutubeDataImport.java
@@ -9,14 +9,20 @@ import org.apache.jena.rdf.model.ResIterator;
 import org.apache.jena.rdf.model.Resource;
 import org.apache.jena.vocabulary.RDF;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.io.File;
+import java.io.IOException;
+import java.util.List;
 
 @Component("youtubeImport")
 public class YoutubeDataImport implements DataImport {
 
     private YoutubeVideoService youtubeVideoService;
+
+    @Value("${dataimport.youtube.max}")
+    private int maxDataImport = 100;
 
     @Autowired
     public void setYoutubeVideoService(YoutubeVideoService youtubeVideoService) {
@@ -30,9 +36,27 @@ public class YoutubeDataImport implements DataImport {
 
         // TODO: RDFize the file
 
-        // FIXME: Testdata
-        Resource res = dataModel.getResource(MusicGraph.entityBaseUri + "elsa");
-        res.addProperty(Schema.name, "Elsa");
+
+        List<YoutubeVideoService.YoutubeVideo> videos = null;
+        try {
+            videos = youtubeVideoService.parseFile(file.getPath());
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+
+        int count = 0;
+        for (YoutubeVideoService.YoutubeVideo video: videos) {
+            Resource res = dataModel.createResource();
+            res.addProperty(RDF.type, MusicGraph.YoutubeVideo);
+            res.addProperty(Schema.name, video.getVideoTitle());
+            res.addProperty(MusicGraph.clickedAt, video.getViewDate());
+
+            count++;
+            if (count >= maxDataImport) {
+                break;
+            }
+        }
 
 
         // TODO: Implement a limit to parse videos
@@ -40,7 +64,23 @@ public class YoutubeDataImport implements DataImport {
         while (itr.hasNext()) {
             Resource video = itr.nextResource();
 
-            dataModel = youtubeVideoService.getMusicVideo(video, dataModel);
+            Resource song = youtubeVideoService.getMusicVideo(video, dataModel);
+            if (song != null) {
+                continue;
+            }
+            Resource artist = youtubeVideoService.getArtist(video, dataModel);
+            if (artist != null) {
+                song = dataModel.createResource();
+                song.addProperty(RDF.type, MusicGraph.YoutubeSongVideo)
+                        .addProperty(Schema.creditedTo, artist);
+                if (video.hasProperty(MusicGraph.clickedAt)) {
+                    song.addProperty(MusicGraph.listenedAt, video.getProperty(MusicGraph.clickedAt).getObject());
+                }
+                if (video.hasProperty(MusicGraph.listenedAt)) {
+                    song.addProperty(MusicGraph.listenedAt, video.getProperty(MusicGraph.listenedAt).getObject());
+                }
+            }
+
 
         }
 
